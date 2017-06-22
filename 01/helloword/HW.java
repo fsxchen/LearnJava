69d83dd11703736814406bf95d82e960f04e1911
arron <fsxchen@gmail.com>
2017-06-22 18:00:43 +0800

    java 初学

---
+++ 01/helloword/HW.java
@@ -0,0 +1,11 @@
+class HelloWord {
+	public static void main(String args[]) {
+		System.out.println("哈喽");
+	}
+}
+class Foo {
+	int i = 0;
+	public static void main(String args[]) {
+		System.out.println("xx");
+	}
+}
